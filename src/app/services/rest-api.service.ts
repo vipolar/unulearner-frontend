b86cab599917b906e2aed988b36bf2091d92783a
vipolar <miskuzius@gmail.com>
2023-10-28 09:01:53 +0200

    we be migrating

--- src/app/services/rest-api.service.ts
+++ src/app/services/rest-api.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class RestAPIService {
 
-  constructor() { }
-}
+    constructor(private http: HttpClient) { }
+
+    httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    };
+
+    postUserRegistrationForm(userCredentials: any) {
+        let url = "http://backend.xloodtravel-development.website/users/";
+        return this.http.post(url, userCredentials, this.httpOptions);
+    };
+};
