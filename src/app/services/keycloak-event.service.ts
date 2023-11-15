111be970972cfd1838621bcdd0100443632a83c9
vipolar <miskuzius@gmail.com>
2023-11-15 09:03:15 +0000

    if-ladder to switch case in keycloakEventService

--- src/app/services/keycloak-event.service.ts
+++ src/app/services/keycloak-event.service.ts
@@ -12,30 +12,35 @@ export class KeycloakEventService {
   async initializeKeycloakEventListener() {
     this.keycloak.keycloakEvents$.subscribe({
       next: async (event: KeycloakEvent) => {
-        if (event.type == KeycloakEventType.OnAuthError) {
-          console.log('Keycloak received "OnAuthError" event.');
-        }
-        if (event.type == KeycloakEventType.OnAuthLogout) {
-          console.log('Keycloak received "OnAuthLogout" event.');
-        }
-        if (event.type == KeycloakEventType.OnAuthRefreshError) {
-          console.log('Keycloak received "OnAuthRefreshError" event.');
-        }
-        if (event.type == KeycloakEventType.OnAuthRefreshSuccess) {
-          console.log('Keycloak received "OnAuthRefreshSuccess" event.');
-        }
-        if (event.type == KeycloakEventType.OnAuthSuccess) {
-          console.log('Keycloak received "OnAuthSuccess" event.');
-        }
-        if (event.type == KeycloakEventType.OnReady) {
-          console.log('Keycloak received "OnReady" event.');
-        }
-        if (event.type == KeycloakEventType.OnTokenExpired) {
-          console.log('Keycloak received "OnTokenExpired" event.');
-          await this.keycloak.updateToken(30);
-        }
-        if (event.type == KeycloakEventType.OnActionUpdate) {
-          console.log('Keycloak received "OnActionUpdate" event.');
+        switch (event.type) {
+          case KeycloakEventType.OnAuthError:
+            console.log('Keycloak received "OnAuthError" event.');
+            break;
+          case KeycloakEventType.OnAuthLogout:
+            console.log('Keycloak received "OnAuthLogout" event.');
+            break;
+          case KeycloakEventType.OnAuthRefreshError:
+            console.log('Keycloak received "OnAuthRefreshError" event.');
+            break;
+          case KeycloakEventType.OnAuthRefreshSuccess:
+            console.log('Keycloak received "OnAuthRefreshSuccess" event.')
+            break;
+          case KeycloakEventType.OnAuthSuccess:
+            console.log('Keycloak received "OnAuthSuccess" event.');
+            break;
+          case KeycloakEventType.OnReady:
+            console.log('Keycloak received "OnReady" event.');
+            break;
+          case KeycloakEventType.OnTokenExpired:
+            console.log('Keycloak received "OnTokenExpired" event.');
+            await this.keycloak.updateToken(30);
+            break;
+          case KeycloakEventType.OnActionUpdate:
+            console.log('Keycloak received "OnActionUpdate" event.');
+            break;
+          default:
+            console.error('Keycloak event type unexpected!');
+            break;
         }
       }
     });
